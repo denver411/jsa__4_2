6fff7abfbc21a3bd225d28ca521e72a4b02f9151
Denis Zhebov <denver411@yandex-team.ru>
2019-02-04 20:27:14 +0300

    Доработано после проверки

--- test/app.test.js
+++ test/app.test.js
@@ -1,22 +1,46 @@
 import findBy from '../src/js/app';
 
+const heroesInfo = [{
+  name: 'маг',
+  type: 'character',
+  description: 'Персонаж, обладающий магическими способностями',
+},
+{
+  name: 'заклинание',
+  type: 'attack',
+  description: 'Атака магическим заклинанием',
+},
+{
+  name: 'урон',
+  type: 'help',
+  description: 'Страница описания элемента интерфейса',
+},
+];
 const nameFinder = findBy('name', 'урон');
 const typeFinder = findBy('type', 'attack');
-const wrongFinder = findBy('name', 'attack');
-const results = [
-  { name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями' },
-  { name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием' },
-  { name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса' },
-];
+const wrongNameFinder = findBy('name', 'attack');
+const wrongPropsFinder = findBy('health', 100);
 
 test('Осуществляется поиск по свойству name', () => {
-  expect(results.filter(nameFinder)).toEqual([{ name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса' }]);
+  expect(heroesInfo.filter(nameFinder)).toEqual([{
+    name: 'урон',
+    type: 'help',
+    description: 'Страница описания элемента интерфейса',
+  }]);
 });
 
 test('Осуществляется поиск по свойству type', () => {
-  expect(results.filter(typeFinder)).toEqual([{ name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием' }]);
+  expect(heroesInfo.filter(typeFinder)).toEqual([{
+    name: 'заклинание',
+    type: 'attack',
+    description: 'Атака магическим заклинанием',
+  }]);
 });
 
-test('При неверном значении возвращается пустой массив', () => {
-  expect(results.filter(wrongFinder)).toEqual([]);
+test('При неверном значении свойства возвращается пустой массив', () => {
+  expect(heroesInfo.filter(wrongNameFinder)).toEqual([]);
+});
+
+test('При неверном cвойстве возвращается пустой массив', () => {
+  expect(heroesInfo.filter(wrongPropsFinder)).toEqual([]);
 });
